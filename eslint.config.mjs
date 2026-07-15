8d59c4095723078c56eeb0fe65d01bcfbae9ea7c
kevinshah-dev <shahkevinh@gmail.com>
2026-07-15 18:26:42 -0400

    added cloudflare workers config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,12 @@
 import nextVitals from "eslint-config-next/core-web-vitals"
 import nextTs from "eslint-config-next/typescript"
 
-const eslintConfig = [...nextVitals, ...nextTs]
+const eslintConfig = [
+  {
+    ignores: ["cloudflare-env.d.ts", ".open-next/**"],
+  },
+  ...nextVitals,
+  ...nextTs,
+]
 
 export default eslintConfig
